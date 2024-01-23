eb654c38e12110e61393913b721d591c620bc07d
yashdholam <yashdholam04@gmail.com>
2024-01-23 18:07:16 +0530

    This is First Commint for DBT project

---
+++ dbt/batch2_elt_project/target/compiled/batch2_elt_project/models/fact/fact_products.sql
@@ -0,0 +1,18 @@
+with stg_product_data as
+(
+    select * from dev.silver.stg_products
+)
+
+select  
+ REPLACE(product_id,'"', '') AS product_id ,
+ IFNULL(product_category_name, 'NO NAME ASSIGNED') as product_category_name,
+ IFNULL(product_name_length, 0) as product_name_length,
+ IFNULL(product_description_length, 0)as product_description_length,
+ IFNULL(product_photos_qty, 0) as product_photos_qty,
+ IFNULL(product_weight_g, 0) as product_weight_g ,
+ IFNULL(product_length_cm, 0) as product_length_cm,
+ IFNULL(product_height_cm, 0) as product_height_cm,
+ IFNULL(product_width_cm, 0) as product_width_cm,
+ INSERTED_AT as inserted_at
+ 
+from stg_product_data
---
+++ dbt/batch2_elt_project/target/run/batch2_elt_project/models/staging/stg_products.sql
@@ -0,0 +1,18 @@
+
+  create or replace   view dev.silver.stg_products
+  
+   as (
+    select  product_id ,
+ product_category_name,
+ product_name_length,
+ cast(product_description_length as int)as product_description_length,
+ cast(product_photos_qty as int)as product_photos_qty,
+ cast(product_weight_g as int)as product_weight_g,
+ cast(product_length_cm as int)as product_length_cm,
+ cast(product_height_cm as int)as product_height_cm,
+ cast(product_width_cm as int)as product_width_cm,
+ INSERTED_AT as inserted_at
+ from
+  DEV.BRONZE.products
+  );
+
